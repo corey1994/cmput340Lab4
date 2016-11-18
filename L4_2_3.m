f0f05807d2597bd277309aea4b87c7a2afadb4c2
corey hunt <hunt1@ualberta.ca>
2016-11-18 15:15:14 -0700

    seems to be be achieving the correct position, however the angles are not humanly achieveable

--- L4_2_3.m
+++ L4_2_3.m
@@ -1,16 +1,17 @@
 addpath /cshome/vis/data;
 load human_data;
-load walk1;
+load jump;
 robot3D('new');
 
 thetaL = [0;0;0;0];
 thetaR = [0;0;0;0];
 %For each column in the data
-for i = 1:size(L)
+for i = 1:size(L,2)
 
     thetaL = invKin3D(Ml , thetaL, L(:,i));
     thetaR = invKin3D(Mr , thetaR, R(:,i));
     theta = [thetaL; thetaR];
     robot3D(humanInterp(drad, theta));
+    pause(0.5);
 
 end
